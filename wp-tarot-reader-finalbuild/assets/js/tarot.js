8da3d1cba5b5509755ee3c5205a2bb030de2063b
ayanoguchi <a.noguchi.1985@gmail.com>
2025-04-16 11:47:56 +0900

    Update tarot.js

--- wp-tarot-reader-finalbuild/assets/js/tarot.js
+++ wp-tarot-reader-finalbuild/assets/js/tarot.js
@@ -1,12 +1,16 @@
 jQuery(document).ready(function($){
     const bg = tarotAjax.bg_url;
     if (bg) $('#tarot-wrap').css('background-image', 'url(' + bg + ')');
+
     $('#draw-tarot').on('click', function(){
         $.post(tarotAjax.ajax_url, {
             action: 'tarot_draw',
             post_id: tarotAjax.post_id
         }, function(response){
             let html = '<h3>' + response.card + '</h3>';
+            if(response.image){
+                html = '<img src="' + response.image + '" style="max-width:200px; margin-bottom:10px;"><br>' + html;
+            }
             if(response.meaning) html += '<p>' + response.meaning + '</p>';
             if(response.banner) html += '<div>' + response.banner + '</div>';
             $('#tarot-result').html(html).addClass('show');
