b7c1883f52ed9f1f03d5c9747c3a267660bf9d4d
ayanoguchi <a.noguchi.1985@gmail.com>
2025-04-16 11:47:23 +0900

    Update ajax.php

--- wp-tarot-reader-finalbuild/includes/ajax.php
+++ wp-tarot-reader-finalbuild/includes/ajax.php
@@ -1,5 +1,6 @@
 <?php
 if (!defined('ABSPATH')) exit;
+
 function tarot_enqueue_assets() {
     wp_enqueue_style('tarot-ui', plugins_url('../assets/css/tarot-style.css', __FILE__));
     wp_enqueue_script('tarot-js', plugins_url('../assets/js/tarot.js', __FILE__), ['jquery'], null, true);
@@ -13,18 +14,41 @@ function tarot_enqueue_assets() {
     }
 }
 add_action('wp_enqueue_scripts', 'tarot_enqueue_assets');
+
 add_action('wp_ajax_tarot_draw', 'tarot_handle_draw');
 add_action('wp_ajax_nopriv_tarot_draw', 'tarot_handle_draw');
+
 function tarot_handle_draw() {
-    $jp = ['fool'=>'愚者','magician'=>'魔術師','high_priestess'=>'女教皇','empress'=>'女帝','emperor'=>'皇帝',
-           'hierophant'=>'法王','lovers'=>'恋人','chariot'=>'戦車','strength'=>'力','hermit'=>'隠者',
-           'wheel_of_fortune'=>'運命の輪','justice'=>'正義','hanged_man'=>'吊るされた男','death'=>'死神',
-           'temperance'=>'節制','devil'=>'悪魔','tower'=>'塔','star'=>'星','moon'=>'月','sun'=>'太陽',
-           'judgement'=>'審判','world'=>'世界'];
+    $jp = [
+        'fool'=>'愚者','magician'=>'魔術師','high_priestess'=>'女教皇','empress'=>'女帝','emperor'=>'皇帝',
+        'hierophant'=>'法王','lovers'=>'恋人','chariot'=>'戦車','strength'=>'力','hermit'=>'隠者',
+        'wheel_of_fortune'=>'運命の輪','justice'=>'正義','hanged_man'=>'吊るされた男','death'=>'死神',
+        'temperance'=>'節制','devil'=>'悪魔','tower'=>'塔','star'=>'星','moon'=>'月','sun'=>'太陽',
+        'judgement'=>'審判','world'=>'世界'
+    ];
+    
     $post_id = intval($_POST['post_id'] ?? 0);
     $keys = array_keys($jp);
     $selected = $keys[array_rand($keys)];
     $meaning = get_post_meta($post_id, 'tarot_meaning_' . $selected, true);
     $banner = get_post_meta($post_id, 'tarot_affiliate_html', true);
-    wp_send_json(['card' => $jp[$selected], 'meaning' => $meaning, 'banner' => $banner]);
+
+    // 🔗 カード画像の紐付け（tarot_card 投稿から取得）
+    $args = [
+        'post_type' => 'tarot_card',
+        'name' => $selected,
+        'posts_per_page' => 1
+    ];
+    $cards = get_posts($args);
+    $image_url = '';
+    if ($cards && has_post_thumbnail($cards[0]->ID)) {
+        $image_url = get_the_post_thumbnail_url($cards[0]->ID, 'medium');
+    }
+
+    wp_send_json([
+        'card' => $jp[$selected],
+        'image' => $image_url,
+        'meaning' => $meaning,
+        'banner' => $banner
+    ]);
 }
