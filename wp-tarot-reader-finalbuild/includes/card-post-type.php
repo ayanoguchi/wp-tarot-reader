84e8c4feb8dc7c2343f2218e54774cd8da87eacd
ayanoguchi <a.noguchi.1985@gmail.com>
2025-04-16 13:00:13 +0900

    Update card-post-type.php

--- wp-tarot-reader-finalbuild/includes/card-post-type.php
+++ wp-tarot-reader-finalbuild/includes/card-post-type.php
@@ -1,10 +1,19 @@
 <?php
 if (!defined('ABSPATH')) exit;
+
 function wp_tarot_register_card_post_type() {
     register_post_type('tarot_card', [
-        'labels' => ['name' => 'カード管理', 'singular_name' => 'タロットカード'],
-        'public' => false, 'show_ui' => true, 'show_in_rest' => true,
+        'labels' => [
+            'name' => 'カード管理',
+            'singular_name' => 'タロットカード'
+        ],
+        'public' => false,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'show_in_rest' => true,
         'supports' => ['title', 'thumbnail'],
+        'rewrite' => ['slug' => 'tarot_card'], // 
+        'publicly_queryable' => true,          // 
         'menu_icon' => 'dashicons-images-alt2'
     ]);
 }
